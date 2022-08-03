31ff57402e06bb38757c7785f2585f6825e43a85
Snackme <loc.vinh@hotmail.com>
2022-08-03 14:48:17 +0200

    apostrofe change because site couldn't deploy

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const Page = () => {
   return (
     <Container>
       <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
-        Hello! I'm a computer science student in Norway!
+        Hello! I&apos;m a computer science student in Norway!
       </Box>
 
       <Box display={{ md: 'flex' }}>
