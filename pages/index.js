ca6ea1c614fc1e64eb630c796bb7af588522fe13
Snackme <loc.vinh@hotmail.com>
2022-08-04 15:39:30 +0200

    Fixed profile picture not aligning

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,32 @@
-import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
 
 const Page = () => {
   const pic = `/images/catmotorcycle${useColorModeValue('', '-dark')}.png`
   return (
     <Container>
-      <Box borderRadius="lg" bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")} p={3} mb={6} textalign="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        mb={6}
+        p={3}
+        textalign="center"
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
         Hello! I&apos;m Loc. Welcome to my page!
       </Box>
 
-      <Box>
-        <Box display={{ md: 'flex' }}>
-          <Box flexGrow={1}>
-            <Heading as="h2" variant="page-title">
-              Loc Nguyen
-            </Heading>
-            <p>
-              Computer Science student specializing in ( Java / C / Python )
-            </p>
-          </Box>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Loc Nguyen
+          </Heading>
+          <p>Computer Science student at Oslomet</p>
         </Box>
 
         <Box
@@ -27,7 +36,6 @@ const Page = () => {
           textalign="center"
         >
           <Box
-
             flexShrink={0}
             mt={{ base: 4, md: 0 }}
             ml={{ md: 6 }}
@@ -35,16 +43,16 @@ const Page = () => {
           >
             <Image
               src={pic}
-              borderColor={useColorModeValue("blackAlpha.800", "whiteAlpha.800")}
+              borderColor={useColorModeValue(
+                'blackAlpha.800',
+                'whiteAlpha.800'
+              )}
               borderWidth={2}
               borderStyle="solid"
               maxWidth="100px"
               display="inline-block"
               borderRadius="full"
-              
               alt="Profile Image"
-              width="100px"
-              height="100px"
             />
           </Box>
         </Box>
