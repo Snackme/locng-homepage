087ac6151e15d31cf023bfcc18ce67934da75ebe
Snackme <loc.vinh@hotmail.com>
2022-08-05 15:12:09 +0200

    Updated package-json. Made bio and changed some theme-stuff around. Deleted projects tab cause it was causing issues

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
+import NextLink from 'next/link'
 import {
   Container,
+  Button,
   Box,
   Heading,
   Image,
+  Link,
   useColorModeValue
 } from '@chakra-ui/react'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { BioSection, BioYear } from '../components/layouts/bio'
 
 const Page = () => {
   const pic = `/images/catmotorcycle${useColorModeValue('', '-dark')}.png`
@@ -14,7 +19,7 @@ const Page = () => {
     <Container>
       <Box
         borderRadius="lg"
-        bg={useColorModeValue('blackAlpha.300', 'whiteAlpha.200')}
+        bg={useColorModeValue('blackAlpha.200', 'whiteAlpha.200')}
         mb={6}
         p={3}
         textalign="center"
@@ -63,20 +68,83 @@ const Page = () => {
       </Box>
 
       <Section delay="0.1">
-        <Heading as="h3">
-          <u>About me.</u>
+        <Heading as="h3" variant="section-title">
+          About me.
+        </Heading>
+        <Section>
+          <Paragraph>
+            I&apos;m a computer engineer student, studying at the university of
+            Oslomet. I am currently working towards my bachelor thesis next
+            spring, by learning as much I can about computer engineering as
+            possible.
+          </Paragraph>
+          <Paragraph>
+            I am doing this by making small projects here and there in languages
+            I am interested in, including this portfolio-ish page right here, in
+            the hopes of one day becoming a fullstack engineer! Feel free to
+            snoop around. I&apos;m also available through my various links down
+            below.
+          </Paragraph>
+        </Section>
+
+        <Box align="right" my={4}>
+          <NextLink href="/projects">
+            <Button centerIcon={<ChevronRightIcon />} colorScheme="blue">
+              My projects
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+
+        <BioSection>
+          <BioYear>1999</BioYear>
+          Born in Hanoi, Vietnam
+        </BioSection>
+
+        <BioSection>
+          <BioYear>2019-2020</BioYear>
+          Completed Military service in the Norwegian Army as second
+          team-leader.
+        </BioSection>
+
+        <BioSection>
+          <BioYear>2020 - present</BioYear>
+          Student on a bachelor&apos;s program in Computer Science at the Oslo
+          Metropolitan University
+        </BioSection>
+      </Section>
+
+      <Section delay="0.3">
+        <Heading as="h3" variant="section-title">
+          Hobbies and interests
         </Heading>
         <Paragraph>
-          I&apos;m a computer engineer student, studying at the university
-          Oslomet. I am currently working towards my bachelor thesis next
-          spring, by learning as much I can about computer engineering as
-          possible.
+        -Hiking
         </Paragraph>
+
         <Paragraph>
-          I am doing this by making small projects here and there in languages I
-          am interested in, including this portfolio-ish page right here, in the
-          hopes of one day becoming a fullstack engineer! Feel free to snoop
-          around. I'm also available through my various links down below.
+        -Camping
+        </Paragraph>
+
+        <Paragraph>
+        -Football
+        </Paragraph>
+
+        <Paragraph>
+        -AI
+        </Paragraph>
+
+        <Paragraph>
+        -Data-Security
+        </Paragraph>
+
+        <Paragraph>
+        -SoftwareDesign and development
         </Paragraph>
       </Section>
     </Container>
