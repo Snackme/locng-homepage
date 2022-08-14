a876d94a93adea730b65c79a82e71d0a798fe02f
Snackme <loc.vinh@hotmail.com>
2022-08-14 17:19:41 +0200

    Updated skills

--- components/skills.js
+++ components/skills.js
@@ -1,8 +1,8 @@
 import Image from 'next/image'
-import { Text, useColorModeValue } from '@chakra-ui/react'
+import { Box, Text, useColorModeValue } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 import Section from './section'
-import Paragraph from "../components/paragraph"
+import Paragraph from '../components/paragraph'
 const LogoBox = styled.span`
   font-weight: bold;
   font-size: 18px;
@@ -25,61 +25,35 @@ const Skills = () => {
   return (
     <Section>
       <Section>
-        <Paragraph>These are the languages I&apos;m comfortable with</Paragraph>
-      <LogoBox>
-        <Image src={javaLogo} width={40} height={50} alt="logo"></Image>
-        <Text
-          color={useColorModeValue('gray.800', 'whiteaAlpha.900')}
-          fontFamily="Noto Sans, sans-serif"
-          fontWeight="bold"
-          ml={3}
-        >
-          Java
-        </Text>
-      </LogoBox>
+        <Paragraph>
+          These are the languages I have somewhat mastery over
+        </Paragraph>
+        <LogoBox>
+          <Image src={javaLogo} width={40} height={50} alt="logo"></Image>
+          <Text ml={3}>Java</Text>
+        </LogoBox>
 
-      <LogoBox>
-        <Image src={cLogo} width={40} height={40} alt="logo"></Image>
-        <Text
-          color={useColorModeValue('gray.800', 'whiteaAlpha.900')}
-          fontFamily="Noto Sans, sans-serif"
-          fontWeight="bold"
-          ml={3}
-        >
-          C
-        </Text>
-      </LogoBox>
-
+        <LogoBox>
+          <Image src={cLogo} width={35} height={40} alt="logo"></Image>
+          <Text ml={3}>C</Text>
+        </LogoBox>
       </Section>
 
       <Section>
-        <Paragraph>These are the languages I&apos;m still working on</Paragraph>
+        <Paragraph>
+          These are the languages I&apos;m still working on mastering
+        </Paragraph>
         <LogoBox>
-        <Image src={reactLogo} width={40} height={40} alt="logo"></Image>
-        <Text
-          color={useColorModeValue('gray.800', 'whiteaAlpha.900')}
-          fontFamily="Noto Sans, sans-serif"
-          fontWeight="bold"
-          ml={3}
-        >
-          React
-        </Text>
-      </LogoBox>
+          <Image src={reactLogo} width={40} height={40} alt="logo"></Image>
+          <Text ml={3}>React</Text>
+        </LogoBox>
 
-      <LogoBox>
-        <Image src={pythonLogo} width={40} height={40} alt="logo"></Image>
-        <Text
-          color={useColorModeValue('gray.800', 'whiteaAlpha.900')}
-          fontFamily="Noto Sans, sans-serif"
-          fontWeight="bold"
-          ml={3}
-        >
-          Python
-        </Text>
-      </LogoBox>
+        <LogoBox>
+          <Image src={pythonLogo} width={40} height={40} alt="logo"></Image>
+          <Text ml={3}>Python</Text>
+        </LogoBox>
       </Section>
     </Section>
-    
   )
 }
 
